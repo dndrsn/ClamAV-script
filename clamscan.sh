cfb520df65eb0c26e37b8aedc817654f195d165a
Dan Anderson <dan@vshift.com>
2016-04-04 15:35:12 -0400

    Bunch of fixes to improve stability, close scan timing gap, etc.

--- clamscan.sh
+++ clamscan.sh
@@ -19,21 +19,32 @@ function clamscan_dir {
     if [ -f  "$log_file" ]
     then
         # use newer files then logfile
+        logfile_mod=$(stat -f '%c' $log_file)
+        now=$(date +%s)
+        delta=$(expr $now - $logfile_mod)
+        # search a full day behind to catch any files modified between when the file list was created and the scan finish
+        mod_seconds=$(expr $delta + 86400)
+        mod_min=$(expr $mod_seconds / 60)
+        mod_days=$(expr $mod_min / 60 / 24)
         echo "--- Searching for files modified since the log file was updated: $log_file"
-        find "$scan_dir" -type f -cnewer "$log_file" -print >> "$list_file"
+        echo "--- Searching for files modified in the last $mod_days days"
+        # find "$scan_dir" -type f -cnewer "$log_file" -print >> "$list_file"
+        find "$scan_dir" -mount -type f -cmin $mod_min -print >> "$list_file"
     else
         # scan last 60 days
         echo "--- Searching for files modified in the last 60 days"
-        find "$scan_dir" -type f -ctime -60 -print >> "$list_file"
+        find "$scan_dir" -mount -type f -ctime -60 -print >> "$list_file"
     fi
 
 
     if [ -s "$list_file" ]
     then
         echo "--- Scanning files"
+        echo "--- Temp file is: ${log_file}.tmp"
 
         # Scan files
-        clamscan -i -f "$list_file" > "$log_file"
+        clamscan --quiet --cross-fs=no --follow-dir-symlinks=0 --follow-file-symlinks=0  i -f "$list_file" > "${log_file}.tmp"
+        mv "${log_file}.tmp" "${log_file}"
 
         # If there were infected files detected, send email alert
         if [ `cat $log_file | grep Infected | grep -v 0 | wc -l` != 0 ]
@@ -53,6 +64,12 @@ function clamscan_dir {
     fi
 }
 
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 # update virus database
 echo "--- Updating virus db"
